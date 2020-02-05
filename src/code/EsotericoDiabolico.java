121420b35f25604346eded5a31830b0dbab9718c
Santi <ssabate@iesebre.com>
2020-02-05 11:30:37 +0100

    detecta bé l'exemple de l'enunciat, però quan l'executes retorna Memory limit exceeded (MLE), per tant s'ha d'eliminar algun dels vectors??

--- src/code/EsotericoDiabolico.java
+++ src/code/EsotericoDiabolico.java
@@ -34,7 +34,7 @@ public class EsotericoDiabolico {
                 valors[i] = Integer.parseInt(nums[i]);
             }
 
-            boolean diabolic = true, esoteric=true;
+            boolean diabolic = true, esoteric = true;
             int cm = 0;
             for (int i = 0; i < n * n && diabolic; i = i + n) {
                 int suma = 0;
@@ -65,29 +65,37 @@ public class EsotericoDiabolico {
                 diabolic = (cm == suma);
 
             }
-            
-            if (diabolic) {
-           
-                 int cm2=4*cm/n;
-                 if(esoteric=(cm2==valors[0]+valors[n-1]+valors[n*(n-1)]+valors[valors.length-1])){
-                    if(n%2==0){
-                        //TODO
+
+            if (diabolic && esoteric) {
+
+                int cm2 = 4 * cm / n;
+                if (esoteric = (cm2 == valors[0] + valors[n - 1] + valors[n * (n - 1)] + valors[n * n - 1])) {
+                    if (n % 2 == 0) {
                         //La suma de las dos casillas centrales de cada uno de los cuatro laterales suman el doble de la constante mágica 2 (2 · CM2)
                         //La suma de las cuatro casillas centrales da como resultado la constante mágica 2.
-                        esoteric=(2*cm2==valors[n/2]+valors[n/2-1]+valors[n*n/2]+valors[n*(n/2-1)]+valors[n*n/2-1]+valors[n*(n/2+1)-1]+valors[n*(n-1)+n/2]+valors[n*(n-1)+n/2-1]) &&
-                                (cm2==valors[n*(n/2-1)+n/2]+valors[n*(n/2-1)+n/2-1]+valors[n*n/2+n/2]+valors[n*n/2+n/2-1]);
-                    }else{
-                        //TODO
+                        esoteric = (2 * cm2 == valors[n / 2] + valors[n / 2 - 1] + valors[n * (n / 2)] + valors[n * (n / 2) - 1] + valors[n * (n / 2 - 1)] + valors[n * (n / 2 + 1) - 1] + valors[n * (n - 1) + n / 2] + valors[n * (n - 1) + n / 2 - 1])
+                                && (cm2 == valors[n * (n / 2 - 1) + n / 2] + valors[n * (n / 2 - 1) + n / 2 - 1] + valors[n * (n / 2) + n / 2] + valors[n * (n / 2) + n / 2 - 1]);
+                    } else {
                         //La suma de las cifras de las cuatro casillas de la mitad de los laterales suman la constante mágica 2.
                         //Si se multiplica el valor de la casilla central por 4, se obtiene la constante mágica 2.
-                        esoteric=(cm2==4*valors[n*n/2+n/2+1]) && (cm2==valors[n/2]+valors[n*n/2+1]+valors[n*(n/2+1)-1]+valors[n*(n-1)+n/2]);
+                        esoteric = (cm2 == 4 * valors[n * (n / 2) + n / 2]);
+                        esoteric= esoteric  && (cm2 == valors[n / 2] + valors[n * (n / 2)] + valors[n * (n / 2 + 1) - 1] + valors[n * (n - 1) + n / 2]);
                     }
-                 
-                 }   
-                
-                if(esoteric) System.out.println("ESOTERICO");
-                else System.out.println("DIABOLICO");
-            } else System.out.println("NO");
+
+                }
+
+                if (esoteric) {
+                    System.out.println("ESOTERICO");
+                } else {
+                    System.out.println("DIABOLICO");
+                }
+            } else {
+                if (diabolic) {
+                    System.out.println("DIABOLICO");
+                } else {
+                    System.out.println("NO");
+                }
+            }
 
         } while (true);
 
