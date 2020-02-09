01aa787881613c0caf0ae9260b1b1741b18d6f84
genis99 <gvil7492@gmail.com>
2020-02-09 17:37:01 +0100

    Em dona un error Run-time error

---
+++ src/code/ReescribiendoFracciones.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package code;
+
+import java.util.Scanner;
+
+
+
+/**
+ *
+ * @author alumne
+ */
+public class ReescribiendoFracciones {
+    public static void main(String[] args) {
+        
+        Scanner ent = new Scanner(System.in);
+        int z = 0;
+        while (true) {
+            int sum = 0;
+            z = ent.nextInt();
+            for (int y = 0; y != z && y < 2*z+1; y++) {
+                if ((z*y)%(y-z) == 0) sum += 1;
+            }
+            System.out.println(sum);
+        }
+        
+    }
+}
