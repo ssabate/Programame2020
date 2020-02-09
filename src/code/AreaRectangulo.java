bf5e28309906db35d88a2a3345cf918610e5b3c2
genis99 <gvil7492@gmail.com>
2020-02-09 19:02:52 +0100

    Area de un rectangulo

---
+++ src/code/AreaRectangulo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package code;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumne
+ */
+public class AreaRectangulo {
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+        int x, y, z, t;
+        x = ent.nextInt();
+        y = ent.nextInt();
+        z = ent.nextInt();
+        t = ent.nextInt();
+        while ((x < z || y < t)) {
+            System.out.println((z-x) * (t-y));
+            x = ent.nextInt();
+            y = ent.nextInt();
+            z = ent.nextInt();
+            t = ent.nextInt();
+        }
+        
+    }
+}
