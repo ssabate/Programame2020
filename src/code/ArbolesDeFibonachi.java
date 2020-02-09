ecd93ffec4e9013130a3b97c4138d3a77b042364
jotaela <joanlluisfabra@gmail.com>
2020-02-09 14:00:20 +0100

    arboles de fibonachi, funciona ne local pero no quan ho pujo, el veredicto me retorna un run-time error

---
+++ src/code/ArbolesDeFibonachi.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package code;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author joan
+ */
+public class ArbolesDeFibonachi {
+    static int fill = 0;
+    
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+        ArrayList<Integer> llista = new ArrayList<>();
+        do {
+            String linia = ent.nextLine();
+
+            int n = Integer.parseInt(linia);
+            
+            if (n == -1){
+                break;
+            }
+            
+            llista.add(n);
+
+        } while (true);
+        for (int i = 0; i < llista.size(); i++) {
+            arbFib(llista.get(i));
+            System.out.println("====");
+        }
+    }
+    public static void arbFib (int n) {
+        for (int j = 0; j < fill; j++) {
+            System.out.format("   ");
+        }
+        if (n < 0){
+           return;
+        }
+        if (n == 0 || n == 1)  {
+            System.out.println(n);      
+            return;
+        }
+        System.out.println(fib(n));
+        fill ++;
+        arbFib (n-2);
+        arbFib (n-1);
+        fill = 0;
+    }
+    
+    public static int fib (int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fib (n - 2) + fib (n - 1);
+    }
+}
