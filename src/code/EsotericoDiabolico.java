820bd51cb2c19436c053d45b9140b7d3941a74d1
Santi <ssabate@iesebre.com>
2020-02-11 09:40:53 +0100

    Fibonacci acceptat. El problema era que no dibuixaves bé els arbres

--- src/code/EsotericoDiabolico.java
+++ src/code/EsotericoDiabolico.java
@@ -18,31 +18,37 @@ public class EsotericoDiabolico {
 
         do {
 
-            String linia = ent.nextLine();
+            int n = ent.nextInt();
 
-            if (linia.equals("0")) {
+            if (n==0) {
                 break;
             }
 
-            int n = Integer.parseInt(linia);
-
-            String[] nums = ent.nextLine().split(" ");
-            int[] valors = new int[nums.length];
+            int[][] valors = new int[n][n];
             boolean[] tots = new boolean[n * n];
 
-            for (int i = 0; i < nums.length; i++) {
-                valors[i] = Integer.parseInt(nums[i]);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    valors[i][j]=ent.nextInt();
+                    
+                }
             }
 
             boolean diabolic = true, esoteric = true;
             int cm = 0;
-            for (int i = 0; i < n * n && diabolic; i = i + n) {
-                int suma = 0;
+            int sumaDiag1=0, sumaDiag2=0;
+                
+            for (int i = 0; i < n && diabolic; i++) {
+                int sumaFila = 0, sumaColumna=0;
                 for (int j = 0; j < n && diabolic; j++) {
-                    suma += valors[i + j];
+                    sumaFila += valors[i][j];
+                    sumaColumna += valors[j][i];
+                    if(i==j) sumaDiag1 += valors[i][j];
+                    if(i+j==n-1) sumaDiag2 += valors[i][j];
+
                     try {
-                        if (!tots[valors[i + j] - 1]) {
-                            tots[valors[i + j] - 1] = true;
+                        if (!tots[valors[i][j] - 1]) {
+                            tots[valors[i][j] - 1] = true;
                         } else {
                             diabolic = false;
                         }
@@ -51,35 +57,30 @@ public class EsotericoDiabolico {
                     }
                 }
                 if (cm == 0) {
-                    cm = suma;
+                    cm = sumaFila;
                 } else {
-                    diabolic = (cm == suma);
+                    diabolic = (cm == sumaFila && cm == sumaColumna);
                 }
-
             }
-            for (int i = 0; i < n && diabolic; i++) {
-                int suma = 0;
-                for (int j = 0; j < n * n && diabolic; j = j + n) {
-                    suma += valors[i + j];
-                }
-                diabolic = (cm == suma);
-
-            }
-
+            
+            if(diabolic) diabolic = (cm == sumaDiag1 && cm == sumaDiag2);
+            
             if (diabolic && esoteric) {
 
                 int cm2 = 4 * cm / n;
-                if (esoteric = (cm2 == valors[0] + valors[n - 1] + valors[n * (n - 1)] + valors[n * n - 1])) {
+                //Serà isotèric si les 4 cantonades sumen la 2ª constant màgica
+                if (esoteric = (cm2 == valors[0][0] + valors[0][n - 1] + valors[n - 1][0] + valors[n - 1][n-1])) {
                     if (n % 2 == 0) {
                         //La suma de las dos casillas centrales de cada uno de los cuatro laterales suman el doble de la constante mágica 2 (2 · CM2)
                         //La suma de las cuatro casillas centrales da como resultado la constante mágica 2.
-                        esoteric = (2 * cm2 == valors[n / 2] + valors[n / 2 - 1] + valors[n * (n / 2)] + valors[n * (n / 2) - 1] + valors[n * (n / 2 - 1)] + valors[n * (n / 2 + 1) - 1] + valors[n * (n - 1) + n / 2] + valors[n * (n - 1) + n / 2 - 1])
-                                && (cm2 == valors[n * (n / 2 - 1) + n / 2] + valors[n * (n / 2 - 1) + n / 2 - 1] + valors[n * (n / 2) + n / 2] + valors[n * (n / 2) + n / 2 - 1]);
+                        esoteric = (2 * cm2 == valors[0][n / 2] + valors[0][n / 2 - 1] + valors[(n / 2)][0] + valors[(n / 2) - 1][0] + valors[(n / 2)][n-1] + valors[n / 2- 1][n-1] + 
+                                valors[n-1][ n / 2] + valors[n-1][ n / 2 - 1]);
+                        esoteric = esoteric &&         (cm2 == valors[n/2][n / 2] + valors[n/2][ n / 2 - 1] + valors[n/2-1][ n / 2] + valors[n/2-1][ n / 2 - 1]);
                     } else {
                         //La suma de las cifras de las cuatro casillas de la mitad de los laterales suman la constante mágica 2.
                         //Si se multiplica el valor de la casilla central por 4, se obtiene la constante mágica 2.
-                        esoteric = (cm2 == 4 * valors[n * (n / 2) + n / 2]);
-                        esoteric= esoteric  && (cm2 == valors[n / 2] + valors[n * (n / 2)] + valors[n * (n / 2 + 1) - 1] + valors[n * (n - 1) + n / 2]);
+                        esoteric = (cm2 == 4 * valors[n/2][n / 2]);
+                        esoteric= esoteric  && (cm2 == valors[0][n / 2] + valors[(n / 2)][0] + valors[n/2][n-1] + valors[n-1][ n / 2]);
                     }
 
                 }
